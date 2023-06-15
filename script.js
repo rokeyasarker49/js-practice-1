781ebafd2949a154990e1631dc24bd486f2b52de
Rokeya Sarker <133252712+rokeyasarker49@users.noreply.github.com>
2023-06-15 11:26:13 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+
+var myName = "Rokeya Sarker";
+// console.log(myName);
+
+
+var myName = "Rokeya Sarker";
+var nameOne = "Subah Sadaf";
+//console.log("Total Name List "+ myName + "and" +nameOne);
+
+var name = "Rokeya";
+var nameOne = "Subah Sadaf";
+// console.log(nameOne);
+
+var numOne = "0";
+var numTwo = "20";
+var numThree = "44";
+var numFour = "59";
+var numFive = "34";
+// console.log("Total Number Is - "+numOne+numTwo+numOThree+numFour+numOFive);
+
+
+var numOne = "0";
+var numTwo = "20";
+var numThree = "44";
+var numFour = "59";
+var numFive = "34";
+var totalNum = numOne+numTwo+numThree+numFour+numFive;
+// console.log("Total Number Is - "+totalNum);
+
+
+var tkOne = 20;
+var tkTwo = 50;
+var tkThree = 100;
+//var totalTk = tkOne+tkTwo+tkThree;
+// console.log("Total Tk :"+totalTk);
+
+
+var tkTwo = 50;
+var tkThree = 100;
+var totalTk = tkTwo-tkThree;
+// console.log("Total Tk : "+totalTk);
+
+
+var tkTwo = 50;
+var tkThree = 100;
+// var totalTk = tkTwo*tkThree;
+// console.log("Total Tk : "+totalTk);
+
+
+
+//String To Int
+
+var priceOne = "30";
+var priceTwo = "50";
+var priceThree = "100";
+var price_OneString_ToNum = parseInt(priceOne);
+var price_TwoString_ToNum = parseInt(priceTwo);
+var price_ThreeString_ToNum = parseInt(priceThree);
+//var totalPrice = price_OneString_ToNum+price_ThreeString_ToNum+price_TwoString_ToNum;
+// console.log(totalPrice);
+
+// console.log(price_OneString_ToNum+price_ThreeString_ToNum+price_TwoString_ToNum);
+
+// var totalPrice = price_OneString_ToNum+price_TwoString_ToNum;
+// console.log(totalPrice);
+
+
+
+var price1 = "55.55";
+var price2 = 55;
+// console.log(typeof price1);
+
+
+var priceOne = "55.55";
+var pricetwo = 55;
+// console.log(typeof price2);
+
+var myNameRokeya = true;
+// console.log(typeof myNameRokeya);
+
+
+
+
+var toDay = new Date();
+// console.log(toDay);
+
+
+
+var nameOne = "Tanjia Amrin";
+var nameTwo = "Umme Kulsum";
+var nameThree = "Subah Sadaf";
+var nameFour = "Syeda Shamima";
+var totalName = [nameOne,nameTwo,nameThree,nameFour];
+// console.log(totalName);
+
+
